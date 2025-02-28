488714d96064586bf444b9ccbef6d15f3f884d30
Po-Wei Huang <pohuan@microsoft.com>
2025-02-27 23:31:05 -0800

    Fix python udf

--- tools/pythonpkg/src/python_udf.cpp
+++ tools/pythonpkg/src/python_udf.cpp
@@ -1,6 +1,7 @@
 #include "duckdb/main/query_result.hpp"
 #include "duckdb_python/pybind11/pybind_wrapper.hpp"
 #include "duckdb/function/scalar_function.hpp"
+#include "duckdb/function/udf_function.hpp"
 #include "duckdb_python/pytype.hpp"
 #include "duckdb_python/pyconnection/pyconnection.hpp"
 #include "duckdb_python/pandas/pandas_scan.hpp"
@@ -496,55 +497,55 @@ public:
 		                               nullptr, varargs, function_side_effects, null_handling);
 		return scalar_function;
 	}
-
-	// AVG function copied from test code
-	template <class T>
-	struct udf_avg_state_t {
-		uint64_t count;
-		T sum;
-	};
-
-	struct UDFAverageFunction {
-		template <class STATE>
-		static void Initialize(STATE &state) {
-			state.count = 0;
-			state.sum = 0;
-		}
-
-		template <class INPUT_TYPE, class STATE, class OP>
-		static void Operation(STATE &state, const INPUT_TYPE &input, AggregateUnaryInput &) {
-			state.sum += input;
-			state.count++;
-		}
-
-		template <class INPUT_TYPE, class STATE, class OP>
-		static void ConstantOperation(STATE &state, const INPUT_TYPE &input, AggregateUnaryInput &, idx_t count) {
-			state.count += count;
-			state.sum += input * count;
-		}
-
-		template <class STATE, class OP>
-		static void Combine(const STATE &source, STATE &target, AggregateInputData &) {
-			target.count += source.count;
-			target.sum += source.sum;
-		}
-
-		template <class T, class STATE>
-		static void Finalize(STATE &state, T &target, AggregateFinalizeData &finalize_data) {
-			if (state.count == 0) {
-				finalize_data.ReturnNull();
-			} else {
-				target = state.sum / state.count;
-			}
-		}
-
-		static bool IgnoreNull() {
-			return true;
-		}
-	};
 };
 
 } // namespace
+
+	// AVG function copied from test code
+template <class T>
+struct udf_avg_state_t {
+	uint64_t count;
+	T sum;
+};
+
+struct UDFAverageFunction {
+	template <class STATE>
+	static void Initialize(STATE &state) {
+		state.count = 0;
+		state.sum = 0;
+	}
+
+	template <class INPUT_TYPE, class STATE, class OP>
+	static void Operation(STATE &state, const INPUT_TYPE &input, AggregateUnaryInput &) {
+		state.sum += input;
+		state.count++;
+	}
+
+	template <class INPUT_TYPE, class STATE, class OP>
+	static void ConstantOperation(STATE &state, const INPUT_TYPE &input, AggregateUnaryInput &, idx_t count) {
+		state.count += count;
+		state.sum += input * count;
+	}
+
+	template <class STATE, class OP>
+	static void Combine(const STATE &source, STATE &target, AggregateInputData &) {
+		target.count += source.count;
+		target.sum += source.sum;
+	}
+
+	template <class T, class STATE>
+	static void Finalize(STATE &state, T &target, AggregateFinalizeData &finalize_data) {
+		if (state.count == 0) {
+			finalize_data.ReturnNull();
+		} else {
+			target = state.sum / state.count;
+		}
+	}
+
+	static bool IgnoreNull() {
+		return true;
+	}
+};
 
 ScalarFunction DuckDBPyConnection::CreateScalarUDF(const string &name, const py::function &udf,
                                                    const py::object &parameters,
@@ -576,7 +577,7 @@ AggregateFunction DuckDBPyConnection::CreateAggregateUDF(const string &name, con
 	data.Verify();
 
 	// TODO: Figure out whether I should change it to be similar to GetFunction.
-	return CreateAggregateFunction<UDFAverageFunction, udf_avg_state_t<double>, double, double>("udf_avg_double"));
+	return UDFWrapper::CreateAggregateFunction<UDFAverageFunction, udf_avg_state_t<double>, double, double>("udf_avg_double");
 }
 
 
