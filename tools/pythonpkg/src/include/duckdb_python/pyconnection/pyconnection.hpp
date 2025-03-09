6f2689b22ba5dcc871cc1431f8467473aaed6d81
Po-Wei Huang <pohuan@microsoft.com>
2025-03-08 23:10:16 -0800

    Fix more.

--- tools/pythonpkg/src/include/duckdb_python/pyconnection/pyconnection.hpp
+++ tools/pythonpkg/src/include/duckdb_python/pyconnection/pyconnection.hpp
@@ -406,6 +406,8 @@ private:
 	                               const shared_ptr<DuckDBPyType> &return_type, bool vectorized,
 	                               FunctionNullHandling null_handling, PythonExceptionHandling exception_handling,
 	                               bool side_effects);
+
+	template <typename STATE_TYPE>
 	CombineFuncPtr<double> CreateCombineUDF(const string &name, const py::function &udf,
 	                                const py::object &parameters,
 	                                const shared_ptr<DuckDBPyType> &return_type,
