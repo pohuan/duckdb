4c478e7a61c3ee7810fa3d9968eca31755e1dd88
Po-Wei Huang <pohuan@microsoft.com>
2025-03-09 10:07:07 -0700

    Test more.

--- tools/pythonpkg/src/pyconnection.cpp
+++ tools/pythonpkg/src/pyconnection.cpp
@@ -468,12 +468,6 @@ bool UDFSumFunction::IgnoreNull() {
 	return true;
 }
 
-template <>
-CombineFuncPtr<double>
-DuckDBPyConnection::CreateCombineUDF<double>(const string &name, const py::function &udf, const py::object &parameters,
-                                             const shared_ptr<DuckDBPyType> &return_type, bool vectorized,
-                                             FunctionNullHandling null_handling,
-                                             PythonExceptionHandling exception_handling, bool side_effects);
 
 shared_ptr<DuckDBPyConnection> DuckDBPyConnection::RegisterAggregateUDF(
     const string &name, const py::function &udf, const py::object &arguments,
