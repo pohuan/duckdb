b24c8fb9b73a5802d508f4f98f93a4883ef4f841
Po-Wei Huang <pohuan@microsoft.com>
2025-03-10 22:38:30 -0700

    Fix more.

--- tools/pythonpkg/src/python_udf.cpp
+++ tools/pythonpkg/src/python_udf.cpp
@@ -719,7 +719,7 @@ DuckDBPyConnection::CreateCombineUDF<double>(const string &name, const py::funct
                                              FunctionNullHandling null_handling,
                                              PythonExceptionHandling exception_handling, bool side_effects);
 
-template FinalizeFuncPtrTest<double, double>
+template FinalizeFuncPtr<double, double>
 DuckDBPyConnection::CreateFinalizeUDF<double, double>(
     const string &name, const py::function &udf, const py::object &parameters,
     const shared_ptr<DuckDBPyType> &return_type, bool vectorized, FunctionNullHandling null_handling,
